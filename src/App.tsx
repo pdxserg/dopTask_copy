e4e832ffcd5ad8502ca774864e8c2b37c03c6ac6
SergeyStarchenko <pdxserg@gmail.com>
2024-05-17 10:08:29 -0700

    add task

--- src/App.tsx
+++ src/App.tsx
@@ -177,13 +177,15 @@ function App() {
     }
 
     function addTask(title: string, todolistId: string) {
-        // let task = {id: v1(), title: title, isDone: false};
-        // //достанем нужный массив по todolistId:
-        // let todolistTasks = tasks[todolistId];
-        // // перезапишем в этом объекте массив для нужного тудулиста копией, добавив в начало новую таску:
-        // tasks[todolistId] = [task, ...todolistTasks];
-        // // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-        // setTasks({...tasks});
+    let task = {taskId: v1(), title: title, isDone: false};
+     //   setTodoFromServer(todoFromServer.map(t =>t.todolistId ===todolistId ?{...t, tasks:[task,...t.tasks ]} :t))
+
+        // // //достанем нужный массив по todolistId:
+         //let todolistTasks = todoFromServer.map(t=>t.todolistId === todolistId)
+        // // // перезапишем в этом объекте массив для нужного тудулиста копией, добавив в начало новую таску:
+        // todoFromServer[todolistId] = [task, ...todolistTasks];
+        // // // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
+        // // setTasks({...tasks});
     }
 
     function changeStatus(id: string, isDone: boolean, todolistId: string) {
