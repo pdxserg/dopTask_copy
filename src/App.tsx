0f616e5b75850e8cbb3629f89a66928932884884
SergeyStarchenko <pdxserg@gmail.com>
2024-06-09 19:57:03 -0700

    create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
 
 
 function App() {
-    return (
-        <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    Здесь будет навигация
-                </div>
-                <div className={styles.content}>
-                    Здесь будут кроссовки
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
-        </div>
-    );
+	return (
+		<div>
+			<div className={styles.header}><h1>HEADER</h1></div>
+			<div className={styles.body}>
+				<div className={styles.nav}>
+					Здесь будет навигация
+				</div>
+				<div className={styles.content}>
+					<PageOne/>
+					<PageTwo/>
+					<PageThree/>
+				</div>
+			</div>
+			<div className={styles.footer}>abibas 2023</div>
+		</div>
+	);
 }
 
 
