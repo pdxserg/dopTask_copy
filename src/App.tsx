45872caaa8c992af852d300e8c1cfc1635e24847
SergeyStarchenko <pdxserg@gmail.com>
2024-06-09 20:34:40 -0700

    error

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import styles from "./components/Site.module.css";
 import {PageOne} from "./components/pages/PageOne";
 import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
+import {Route, Routes} from "react-router-dom";
+import {Error404} from "./components/pages/Error404";
 
 
 function App() {
@@ -14,9 +16,17 @@ function App() {
 					Здесь будет навигация
 				</div>
 				<div className={styles.content}>
-					<PageOne/>
-					<PageTwo/>
-					<PageThree/>
+					<Routes>
+
+						<Route element={<PageOne />} path="/1" errorElement={<Error404/>} />
+						<Route element={<PageTwo />} path="/2"/>
+						<Route element={<PageThree />} path="/3"/>
+						<Route element={<Error404 />} path ="/*"/>
+
+
+
+					</Routes>
+
 				</div>
 			</div>
 			<div className={styles.footer}>abibas 2023</div>
