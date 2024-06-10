29809d7e9348ada7b038dd26e0d15a650757706f
SergeyStarchenko <pdxserg@gmail.com>
2024-06-10 14:07:06 -0700

    styled

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404";
 import {NavWrapper} from "./_styles";
 
- const PATH ={
-	 PAGE1: '/page1',
-	 PAGE2: '/page2',
-	 PAGE3: '/page3',
-	 ERROR404: '/page/error404'
- } as const
+const PATH = {
+	PAGE1: '/page1',
+	PAGE2: '/page2',
+	PAGE3: '/page3',
+	ERROR404: '/page/error404'
+} as const
+
 function App() {
 	return (
 		<div>
@@ -20,13 +21,13 @@ function App() {
 			<div className={styles.body}>
 				<div className={styles.nav}>
 					<NavWrapper><NavLink to={PATH.PAGE1}
-					                      // className={({isActive})=>isActive? styles.active :styles.noactive}
+						// className={({isActive})=>isActive? styles.active :styles.noactive}
 					>Adidas</NavLink></NavWrapper>
 					<NavWrapper><NavLink to={PATH.PAGE2}
-					              // className={({isActive})=>isActive? styles.active:styles.noactive}
+						// className={({isActive})=>isActive? styles.active:styles.noactive}
 					>Puma</NavLink></NavWrapper>
 					<NavWrapper><NavLink to={PATH.PAGE3}
-					              // className={({isActive})=>isActive? styles.active:styles.noactive}
+						// className={({isActive})=>isActive? styles.active:styles.noactive}
 					>Nick</NavLink></NavWrapper>
 
 				</div>
@@ -37,8 +38,8 @@ function App() {
 						<Route element={<PageTwo/>} path={PATH.PAGE2}/>
 						<Route element={<PageThree/>} path={PATH.PAGE3}/>
 
+						<Route  path="/*" element={<Error404/>}> </Route>
 
-						<Route path="/*" element={<Navigate to={PATH.ERROR404} />}/>
 						{/*<Route path="/*" element={<Navigate to={PATH.ERROR404}/>}/>*/}
 						{/*<Route element={<Error404/>} path={PATH.ERROR404}/>*/}
 
