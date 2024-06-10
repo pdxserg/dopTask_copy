891f1083554c2591549dcc0fc1a085881ee7e39f
SergeyStarchenko <pdxserg@gmail.com>
2024-06-10 13:47:50 -0700

    styled

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,40 @@ import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404";
 import {NavWrapper} from "./_styles";
 
-
+ const PATH ={
+	 PAGE1: '/page1',
+	 PAGE2: '/page2',
+	 PAGE3: '/page3',
+	 ERROR404: '/page/error404'
+ } as const
 function App() {
 	return (
 		<div>
 			<div className={styles.header}><h1>HEADER</h1></div>
 			<div className={styles.body}>
 				<div className={styles.nav}>
-					<NavWrapper><NavLink to={"/1"}
+					<NavWrapper><NavLink to={PATH.PAGE1}
 					                      // className={({isActive})=>isActive? styles.active :styles.noactive}
 					>Adidas</NavLink></NavWrapper>
-					<NavWrapper><NavLink to={"/2"}
+					<NavWrapper><NavLink to={PATH.PAGE2}
 					              // className={({isActive})=>isActive? styles.active:styles.noactive}
 					>Puma</NavLink></NavWrapper>
-					<NavWrapper><NavLink to={"/3"}
+					<NavWrapper><NavLink to={PATH.PAGE3}
 					              // className={({isActive})=>isActive? styles.active:styles.noactive}
 					>Nick</NavLink></NavWrapper>
 
 				</div>
 				<div className={styles.content}>
 					<Routes>
-						<Route path="/" element={<Navigate to={"/1"}/>}/>
-						<Route element={<PageOne/>} path="/1"/>
-						<Route element={<PageTwo/>} path="/2"/>
-						<Route element={<PageThree/>} path="/3"/>
+						<Route path="/" element={<Navigate to={PATH.PAGE1}/>}/>
+						<Route element={<PageOne/>} path={PATH.PAGE1}/>
+						<Route element={<PageTwo/>} path={PATH.PAGE2}/>
+						<Route element={<PageThree/>} path={PATH.PAGE3}/>
 
-						<Route element={<Error404/>} path={"error404"}/>
-						<Route path="/*" element={<Navigate to={"error404"}/>}/>
 
+						<Route path="/*" element={<Navigate to={PATH.ERROR404} />}/>
+						{/*<Route path="/*" element={<Navigate to={PATH.ERROR404}/>}/>*/}
+						{/*<Route element={<Error404/>} path={PATH.ERROR404}/>*/}
 
 					</Routes>
 
